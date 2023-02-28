1adb14d265391ca180e810ac6d7509798113bb27
Pavan Pothamsetti <ptksed032@gmail.com>
2023-02-28 16:01:59 +0000

    visulaization added

--- CVRP.py
+++ CVRP.py
@@ -9,6 +9,18 @@ import time
 import random
 import cvrplib
 
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def edge_gen(list):
+    edge_list = []
+    for i in range(len(list)-1):
+        edge_list.append((list[i],list[i+1]))
+    return edge_list
+
+colors = [ "#58FF33", "#CD6155", "#DAF7A6", "#FFC300", "#A569BD", "#5499C7", "#45B39D", "#6E2C00", "#FF33D1", "#FFFFFF", "#000000", "#33FFAF", "#33FFE0", "#FF3333"]
+
 instance, solution = cvrplib.download('A-n32-k5',solution=True)
 n=instance.dimension
 strp=instance.name.partition("k")[2]
@@ -47,22 +59,22 @@ for r in range(p):
 import dimod
 B=2*n
 
-T_max=max(2*n-p+1,1)
+# T_max=max(2*n-p+1,1)
 
-t=[[None]*(p) for _ in range(n)]
+# t=[[None]*(p) for _ in range(n)]
 
-for i in range(n):
-    for r in range(p):
-        t[i][r]=dimod.Integer(lower_bound=1,upper_bound=T_max,label=f't.{i}.{r}')
+# for i in range(n):
+#     for r in range(p):
+#         t[i][r]=dimod.Integer(lower_bound=1,upper_bound=T_max,label=f't.{i}.{r}')
 
-for i in range(1,n):
-    for j in range(1,n):
-        if i!=j:
-            for k in range(p):
-                cqm.add_constraint((t[j][k]-(t[i][k]+1)+B*(1-x[k][i][j]))>=0)
+# for i in range(1,n):
+#     for j in range(1,n):
+#         if i!=j:
+#             for k in range(p):
+#                 cqm.add_constraint((t[j][k]-(t[i][k]+1)+B*(1-x[k][i][j]))>=0)
 
 def get_token():
-    return 'DEV-ad327c1fec4c7793764df612eaa1910b4a1f910e'
+    return 'DEV-84bd4a159c19999c61d45bf77755d8a37754ba12'
 
 
 print("Starting D wave")
@@ -87,17 +99,35 @@ for key,val in best_solution.items():
             truck_stops.append(key.split('_')[1:])
             routes[int(truck_stops[-1][0])].append(truck_stops[-1][1:])
 
+paths = []
+
 total_cost=0
 for i in range(p):
     current_route=routes[i]
     current_cost=0
+    temp = []
     for r in current_route:
         current_cost+=c[int(r[0])][int(r[1])]
     print("\nTruck",i,"route:")
     for r in current_route:
         print(r)
+        temp.append(r[0])
     print("\nTruck",i,"cost:",current_cost)
     total_cost+=current_cost
+    paths.append(temp)
+
+G = nx.Graph()
+for i in range(len(instance.coordinates)):
+    G.add_node(str(i),pos=tuple(instance.coordinates[i]))
+for l in paths:
+    G.add_edges_from(edge_gen(l))
+
+nx.draw(G, nx.get_node_attributes(G, 'pos'), with_labels=True, node_size=10)
+
+for l in range(len(paths)):
+    k = G.subgraph(paths[l])
+    nx.draw_networkx(k, nx.get_node_attributes(G, 'pos'), with_labels=True, edge_color=colors[l])
+plt.savefig("solution", bbox_inches=None)
 
 print("\nTotal cost:\t",total_cost)
 
