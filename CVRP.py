2facb4294377333a1e29c1599711b2c56b943871
Sushil Mario <sushilmario@gmail.com>
2023-03-07 16:13:33 +0000

    Add sequence of steps constraint

--- CVRP.py
+++ CVRP.py
@@ -1,5 +1,5 @@
 import numpy as np
-from dimod import ConstrainedQuadraticModel, Binary
+from dimod import ConstrainedQuadraticModel, Binary, Integer
 from dimod import quicksum
 from dwave.system import LeapHybridCQMSampler
 from itertools import chain, combinations
@@ -40,7 +40,7 @@ V=int(n*(n-1)*p)
 D=instance.demands
 Q=instance.capacity
 
-x=[[[Binary(f'x_{r}_{i}_{j}') for j in range(n)] for i in range(n)] for r in range(p)]
+x = np.array([[[Binary(f'x_{r}_{i}_{j}') for j in range(n)] for i in range(n)] for r in range(p)])
 
 cqm = ConstrainedQuadraticModel()
 
@@ -55,24 +55,49 @@ for r in range(p):
     cqm.add_constraint(quicksum(x[r][0][j] for j in range(1,n))==1, label=f'Constraint2_{r}')
 
 # The order of the route is valid and maintained
+
 for j in range(n):
     for r in range(p):
         cqm.add_constraint(quicksum(x[r][i][j] for i in range(n) if i!=j)-quicksum(x[r][j][i] for i in range(n) if i!=j)==0,label=f'Constraint3_{j}_{r}')
+
+B = n
+
+s_max = n
+
+s =[[None]*(p) for _ in range(n)]
+
+for i in range(n):
+    for r in range(p):
+        s[i][r] = Integer(lower_bound=1,upper_bound = s_max,label=f't.{i}.{r}')
+
+for i in range(1,n):
+    for j in range(1,n):
+        if i!=j:
+            for r in range(p):
+                cqm.add_constraint((s[j][r]-(s[i][r]+1)+B*(1-x[r][i][j]))>=0)
 
 # Each vehicle does not exceed its capacity
 for r in range(p):
     cqm.add_constraint(quicksum(D[j]*x[r][i][j] for i in range(n) for j in range(1,n) if j!=i)<=Q,label=f'Constraint4_{r}')
 
-# Sub-tours are prevented from being included in the solution
-for s in p_t_2_plus:
-    for subset in s:
-        cqm.add_constraint(quicksum( \
-                                x[r][i][j] \
-                                for j in subset \
-                                for i in subset \
-                                if i != j \
-                                for k in range(p) \
-                            ) <= len(subset) - 1)
+# # Sub-tours are prevented from being included in the solution
+# def constraint_5(X, subset):
+#     sum = 0
+        
+#     for r in range(p):
+#         for i in subset:
+#             for j in subset:
+#                 if i != j:
+#                     sum += X[r][i][j] 
+
+#     return sum
+
+# for s in p_t_2_plus:
+#     for subset in s:
+#         cqm.add_constraint(constraint_5(X=x, subset = subset) \
+#                            <= len(subset) - 1)
+
+
 
 def get_token():
     return 'DEV-78aeea4bb135991b8b476e1a4a09307a3d458858'
